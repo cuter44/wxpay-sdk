d81129929f9e531158917092a8e343478d129ff3
cuter44 <cuter44@foxmail.com>
2015-07-17 16:58:52 +0800

    + featured: UserInfo

---
+++ src/com/github/cuter44/wxmp/reqs/UserInfo.java
@@ -0,0 +1,95 @@
+package com.github.cuter44.wxmp.reqs;
+
+import java.util.Properties;
+import java.util.List;
+import java.util.Arrays;
+import java.io.IOException;
+
+import com.github.cuter44.wxmp.resps.*;
+
+/** 获取用户基本信息(UnionID机制)
+ * <br />
+ * <a href="http://mp.weixin.qq.com/wiki/14/bb5031008f1494a59c6f71fa0f319c66.html">ref ↗</a>
+ * <br />
+ * <pre style="font-size:12px">
+    参数说明
+    access_token    调用接口凭证
+    openid          普通用户的标识，对当前公众号唯一
+    lang            返回国家地区语言版本，zh_CN 简体，zh_TW 繁体，en 英语
+ * </pre>
+ */
+public class UserInfo extends WxmpRequestBase
+{
+  // KEYS
+    protected static final List<String> KEYS_PARAM = Arrays.asList(
+        "access_token", "openid", "lang"
+    );
+
+    protected static final String KEY_ACCESS_TOKEN  = "access_token";
+    protected static final String KEY_OPENID        = "openid";
+    protected static final String KEY_LANG          = "lang";
+
+    public static final String URL_API_BASE = "https://api.weixin.qq.com/cgi-bin/user/info";
+
+  // CONSTRUCT
+    public UserInfo(Properties prop)
+    {
+        super(prop);
+
+        if (super.getProperty(KEY_LANG) == null)
+            super.setProperty(KEY_LANG, "zh_CN");
+
+        return;
+    }
+
+    public UserInfo(String accessToken, String openid)
+    {
+        super(new Properties());
+
+        this.setProperty(KEY_ACCESS_TOKEN   , accessToken   )
+            .setProperty(KEY_OPENID         , openid        )
+            .setProperty(KEY_LANG           , "zh_CN"       );
+
+        return;
+    }
+
+    public UserInfo(SnsOAuthAccessTokenResponse resp)
+    {
+        super(new Properties());
+
+        super.setProperty(KEY_ACCESS_TOKEN  , resp.getAccessToken() )
+            .setProperty(KEY_OPENID         , resp.getOpenid()      )
+            .setProperty(KEY_LANG           , "zh_CN"               );
+
+        return;
+    }
+
+  // BUILD
+    @Override
+    public UserInfo build()
+    {
+        return(this);
+    }
+
+  // TO_URL
+    @Override
+    public String toURL()
+    {
+        throw(
+            new UnsupportedOperationException("This request does not execute on client side.")
+        );
+    }
+
+  // EXECUTE
+    @Override
+    public UserInfoResponse execute()
+        throws IOException
+    {
+        String url = URL_API_BASE+"?"+super.toQueryString(KEYS_PARAM);
+
+        String respJson = super.executeGet(url);
+
+        return(new UserInfoResponse(respJson));
+    }
+
+}
---
+++ src/com/github/cuter44/wxmp/resps/UserInfoResponse.java
@@ -0,0 +1,74 @@
+package com.github.cuter44.wxmp.resps;
+
+import java.util.Date;
+
+import com.alibaba.fastjson.*;
+
+/** 获取用户基本信息(UnionID机制)
+ * <br />
+ * <pre style="font-size:12px">
+    返回说明
+    <i>inherited from SnsUserinfoResponse</i>
+    openid      用户的唯一标识
+    nickname    用户昵称
+    sex         用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
+    province    用户个人资料填写的省份
+    city        普通用户个人资料填写的城市
+    country     国家，如中国为CN
+    headimgurl  用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空。若用户更换头像，原有头像URL将失效。
+    privilege   用户特权信息，json 数组，如微信沃卡用户为（chinaunicom）
+    unionid     只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段。详见：获取用户个人信息（UnionID机制）
+    <i>Available in UserInfoResponse</i>
+    language 	    用户的语言，简体中文为zh_CN
+    subscribe       用户是否订阅该公众号标识，值为0时，代表此用户没有关注该公众号，拉取不到其余信息。
+    subscribe_time  用户关注时间，为时间戳。如果用户曾多次关注，则取最后关注时间
+    remark          公众号运营者对粉丝的备注，公众号运营者可在微信公众平台用户管理界面对粉丝添加备注
+    gorupid         用户所在的分组ID
+ * </pre>
+ */
+public class UserInfoResponse extends SnsUserinfoResponse
+{
+  // CONSTANTS
+    public static final String KEY_SUBSCRIBE        = "subscribe";
+    public static final String KEY_SUBSCRIBE_TIME   = "subscribe_time";
+    public static final String KEY_GROUPID          = "groupid";
+
+  // CONSTRUCT
+    public UserInfoResponse(String jsonString)
+    {
+        super(jsonString);
+
+        return;
+    }
+
+  // ACCESSOR
+    public Boolean getSubscribe()
+    {
+        return(
+            Boolean.valueOf(
+                super.getProperty(KEY_SUBSCRIBE)
+            )
+        );
+    }
+
+    public Date getSubscribeTime()
+    {
+        return(
+            new Date(
+                Long.valueOf(
+                    super.getProperty(KEY_SUBSCRIBE_TIME)
+                )*1000L
+            )
+        );
+    }
+
+    public Integer getGroupid()
+    {
+        return(
+            Integer.valueOf(
+                super.getProperty(KEY_SUBSCRIBE_TIME)
+            )
+        );
+    }
+
+}
