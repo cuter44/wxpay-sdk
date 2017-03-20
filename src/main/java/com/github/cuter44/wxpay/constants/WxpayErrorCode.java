9778878393e9a0b6583953a494d5187e96f59bb5
cuter44 <cuter44@foxmail.com>
2017-03-20 10:43:33 +0800

    * wxpay:patch:WxpayErrorCode:add new uncovered error code.

--- src/main/java/com/github/cuter44/wxpay/constants/WxpayErrorCode.java
+++ src/main/java/com/github/cuter44/wxpay/constants/WxpayErrorCode.java
@@ -6,20 +6,22 @@ import java.util.HashMap;
 public enum WxpayErrorCode
 {
   // ENUM
-    SYSTEMERROR             (-1, "接口后台错误"),
-    INVALID_TRANSACTIONID   (-2, "无效 transaction_id"),
-    PARAM_ERROR             (-3, "提交参数错误"),
-    ORDERPAID               (-4, "订单已支付"),
-    OUT_TRADE_NO_USED       (-5, "商户订单号重复"),
-    NOAUTH                  (-6, "商户无权限"),
-    NOTENOUGH               (-7, "余额不足"),
-    NOTSUPORTCARD           (-8, "不支持卡类型"),
-    ORDERCLOSED             (-9, "订单已关闭"),
-    BANKERROR               (-10, "银行系统异常"),
-    REFUND_FEE_INVALID      (-11, "退款金额大于支付金额"),
-    ORDERNOTEXIST           (-12, "订单不存在"),
-    REFUNDNOTEXIST          (-13, "退款项不存在"),
-    ERROR                   (-127, "UNKNOWN");
+    SYSTEMERROR                 (-1     , "接口后台错误"                        ),
+    INVALID_TRANSACTIONID       (-2     , "无效 transaction_id"                 ),
+    PARAM_ERROR                 (-3     , "提交参数错误"                        ),
+    ORDERPAID                   (-4     , "订单已支付"                          ),
+    OUT_TRADE_NO_USED           (-5     , "商户订单号重复"                      ),
+    NOAUTH                      (-6     , "商户无权限"                          ),
+    NOTENOUGH                   (-7     , "余额不足"                            ),
+    NOTSUPORTCARD               (-8     , "不支持卡类型"                        ),
+    ORDERCLOSED                 (-9     , "订单已关闭"                          ),
+    BANKERROR                   (-10    , "银行系统异常"                        ),
+    REFUND_FEE_INVALID          (-11    , "退款金额大于支付金额"                ),
+    ORDERNOTEXIST               (-12    , "订单不存在"                          ),
+    REFUNDNOTEXIST              (-13    , "退款项不存在"                        ),
+    PRODUCT_AUTHORITY_UNOPEN    (-14    , "功能未开通"                          ),
+    NO_OAUTH                    (-14    , "此请求可能存在风险，已被微信拦截"    ),
+    ERROR                       (-127   , "UNKNOWN"                             );
 
   // FIELDS
     private int code;
